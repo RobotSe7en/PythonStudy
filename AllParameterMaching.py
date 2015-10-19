c69d9f1ba348154a52ff81b82b24c2a582a68429
RobotSe7en <wangpeng@wangpengdeMacBook-Air.local>
2015-10-19 16:04:41 +0800

    all kinds of arguments matching

---
+++ AllParameterMaching.py
@@ -0,0 +1,25 @@
+def f1(a,b):
+	print(a,b)
+
+def f2(a,*b):
+	print(a,b)
+
+def f3(a,**b):
+	print(a,b)
+
+def f4(a,*b,**c):
+	print(a,b,c)
+
+def f5(a,b=2,c=3):
+	print(a,b,c)
+
+def f6(a,b=2,*c):
+	print(a,b,c)
+
+f1(1,2)
+f1(b=1,a=2)
+f2(1,2,3,4)
+f3(1,x=2,y=3)
+f4(1,2,3,x=2,y=3)
+f5(1,4)
+f6(1,3,4)
